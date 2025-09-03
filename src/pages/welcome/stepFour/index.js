6859c6a6c48e376ed03048ac94402965bbfec0c7
Linda Do <lindado_98@hotmail.com>
2025-09-03 15:55:52 +0200

    updated bio-stepFour

--- src/pages/welcome/stepFour/index.js
+++ src/pages/welcome/stepFour/index.js
@@ -2,14 +2,24 @@ import Form from '../../../components/form';
 
 const StepFour = ({ data, setData }) => {
   return (
+    
     <>
-      <div className="welcome-formheader">
-        <h3>Bio</h3>
+     <div className="welcome-formheader">
+        <h3 className='bio-heading'>Bio</h3>
       </div>
       <Form className="welcome-form">
         <div className="welcome-form-inputs">
-          <textarea name="bio" value={data.bio} onChange={setData}></textarea>
-          <p className="text-blue1">*Required</p>
+          <label className='bio-label'>Bio</label>
+          <textarea 
+          name="bio" 
+          value={data.bio} 
+          onChange={setData} 
+          placeholder='Tell us about yourself, your professional and educational highlights to date...' 
+          maxLength={300}></textarea>
+         <div 
+         className={`welcome-counter ${data.bio.length === 300 ? 'limit-reached' : ''}`}>
+          {data.bio.length}/{300} 
+      </div>
         </div>
       </Form>
     </>
