0ddf2e0b10c66053545dec3f8d8ef499349ccdcc
Linda Do <lindado_98@hotmail.com>
2025-09-10 14:03:48 +0200

    refactor

--- src/pages/welcome/stepOne/index.js
+++ src/pages/welcome/stepOne/index.js
@@ -29,54 +29,39 @@ const StepOne = ({ data, setData, handleFileChange }) => {
                 className="welcome-form-profileimg-input"
                 style={{ width: '80px', marginTop: '10px'}}/>
             ) : ( <ProfileIcon colour="#28C846" background="#64DC78" />)}
+            <Popup trigger={
+                <button className="addHeadshot">
+                  {data.photo ? "Replace headshot" : "Add headshot"}
+                </button> } modal>
+              {close => (
+                <Card>
+                  <div className="welcome-form-popup">
+                    <h3 className="welcome-form-popup-wrapper">
+                      {data.photo ? "Replace Photo" : "Upload Photo"}
+                    </h3>
+                    <p className="text-blue1">Choose a file to upload your headshot</p>
 
-            {data.photo ? ( 
-              <Popup trigger={<button className="addHeadshot">Replace headshot</button>} modal>
-                {close => (
-                  <Card>
-                    <div className="welcome-form-popup">
-                      <h3 className='welcome-form-popup-wrapper'>Upload Photo</h3>
-                      <p className='text-blue1'>Choose a file to upload your headshot</p>
-                        <div className='welcome-form-popup-buttons'>
-                          <button className="offwhite" onClick={() => close()}>Cancel</button>
-                          <input
-                            type="file"
-                            accept="image/*"
-                            style={{ display: "none" }}
-                            id="contained-button-file"
-                            onChange={(e) => handleFileChange(e, close)}/>
-                          <label htmlFor="contained-button-file" className="upload-label">
-                            Upload photo
-                          </label>
-                        </div>
-                      </div>
-                    </Card>
-                  )}
-              </Popup>) : (
-               <Popup trigger={<button className="addHeadshot">Add headshot</button>} modal>
-                  {close => (
-                    <Card>
-                    <div className="welcome-form-popup">
-                      <h3 className='welcome-form-popup-wrapper'>Upload Photo</h3>
-                      <p className='text-blue1'>Choose a file to upload your headshot</p>
-                        <div className='welcome-form-popup-buttons'>
-                          <button className="offwhite" onClick={() => close()}>Cancel</button>
-                          <input
-                            type="file"
-                            accept="image/*"
-                            style={{ display: "none" }}
-                            id="contained-button-file"
-                            onChange={(e) => handleFileChange(e, close)}/>
-                          <label htmlFor="contained-button-file" className="upload-label">
-                            Upload photo
-                          </label>
-                       </div>
-                      
-                      </div>
-                    </Card>
-                    )}
-                </Popup>
-            )}
+                    <div className="welcome-form-popup-buttons">
+                      <button className="offwhite" onClick={() => close()}>
+                        Cancel
+                      </button>
+
+                      <input
+                        type="file"
+                        accept="image/*"
+                        style={{ display: "none" }}
+                        id="contained-button-file"
+                        onChange={(e) => handleFileChange(e, close)}
+                      />
+                      <label htmlFor="contained-button-file" className="upload-label">
+                        Upload photo
+                      </label>
+                    </div>
+                  </div>
+                </Card>
+              )}
+            </Popup>
+
           </div>
 
           <p className="welcome-form-profileimg-error">Please upload a valid image file</p>
