11f32c0f5b349b21ca71f245d0c5a7178f27b0c2
Linda Do <lindado_98@hotmail.com>
2025-09-11 15:27:21 +0200

    fixed bug

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import CohortIcon from '../../assets/icons/cohortIcon';
 import HomeIcon from '../../assets/icons/homeIcon';
 import ProfileIcon from '../../assets/icons/profileIcon';
@@ -7,11 +7,39 @@ import './style.css';
 
 const Navigation = () => {
   const { token } = useAuth();
+  const location = useLocation();
+  
 
   if (!token) {
     return null;
   }
-
+   if (location.pathname === '/welcome') {
+    return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/" onClick={e => e.preventDefault()}>
+            <HomeIcon colour="#000046" />
+            <p>Home</p>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/" onClick={e => e.preventDefault()}>
+            <ProfileIcon />
+            <p>Profile</p>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/cohorts" onClick={e => e.preventDefault()}>
+            <CohortIcon />
+            <p>Cohort</p>
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+    )
+  }
+  
   return (
     <nav>
       <ul>
