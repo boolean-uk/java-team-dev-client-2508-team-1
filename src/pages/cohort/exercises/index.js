afbbbaafa3a76c603ead5155f253c685f720590b
Moueed Ali <moueed.ali@hotmail.com>
2025-09-11 11:15:48 +0200

    fix/fixed issue from Daves last comment

--- src/pages/cohort/exercises/index.js
+++ src/pages/cohort/exercises/index.js
@@ -5,12 +5,25 @@ const Exercises = () => {
     return (
         <>
         <Card>
-          <h4>My Exercises</h4>
-          <section className={`post-interactions-container border-top`}></section>
-          <p className="paragraph-text">Modules:&nbsp;&nbsp;&nbsp;&nbsp; 2/7 completed</p>
-          <p className="paragraph-text">Units:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 4/10 completed</p>
-          <p className="paragraph-text">Exercise:&nbsp;&nbsp;&nbsp;&nbsp; 34/58 completed</p>
-          <button className="see-more-button">See exercises</button>
+            <h4>My Exercises</h4>
+            <section className="post-interactions-container border-top"></section>
+
+            <div className="exercise-row">
+                <span className="label">Modules:</span>
+                <span className="value">2/7 completed</span>
+            </div>
+
+            <div className="exercise-row">
+                <span className="label">Units:</span>
+                <span className="value">4/10 completed</span>
+            </div>
+
+            <div className="exercise-row">
+                <span className="label">Exercise:</span>
+                <span className="value">34/58 completed</span>
+            </div>
+
+            <button className="see-more-button">See exercises</button>
         </Card>
         </>
     )
