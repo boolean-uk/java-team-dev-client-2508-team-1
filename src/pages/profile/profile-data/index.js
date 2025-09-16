243a9ecda089d2ff5b00b8ac8d10a8c0dc2049e6
Richard-Persson <ricpersson00@gmail.com>
2025-09-16 15:34:42 +0200

    removed cohort from profile page

--- src/pages/profile/profile-data/index.js
+++ src/pages/profile/profile-data/index.js
@@ -20,7 +20,6 @@ const ProfileData = () => {
   if (!user) return <div>Loading...</div>;
 
   const { email } = user;
-  const { id } = user.cohort  
   const { name } = user.roles[0]
   const { firstName, lastName, githubUrl, mobile, specialism, bio } = user.profile;
 
@@ -31,7 +30,6 @@ const ProfileData = () => {
       <p className="bio">Bio: {bio} </p>
       <p className="mobile">Mobile: {mobile}</p>
       <p className="githubURL">Github URL: {githubUrl}</p>
-      <p className="cohort">Cohort: {id}</p>
       <p className="specialism">Specialism: {specialism}</p>
       <p className="role">Role: {name}</p>
     </main>
