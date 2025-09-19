85bf3934cdfc56f062a4236523ecf01267c1a5d7
Isabell <isabell.tran@no.experis.com>
2025-09-19 11:37:00 +0200

    Fixed ClickOutside

--- src/components/profile-icon/index.js
+++ src/components/profile-icon/index.js
@@ -2,7 +2,6 @@
 import Popup from 'reactjs-popup';
 import './style.css';
 import SeeProfile from '../seeProfile';
-import Popup from 'reactjs-popup';
 const UserIcon = ({ id, initials, firstname, lastname, role}) => {
 
     const styleGuideColors = [
