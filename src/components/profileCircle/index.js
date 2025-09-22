8c782d4629d1e4f4d3bee360bbfd5bf7a0a67339
Marie Helene Hansen <marhelhan@icloud.com>
2025-09-22 10:42:30 +0200

    fix dashboard - my cohort

--- src/components/profileCircle/index.js
+++ src/components/profileCircle/index.js
@@ -51,5 +51,3 @@ export const CascadingMenu = ({ id }) => {
 };
 
 export default ProfileCircle;
-export { CascadingMenu };
-
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Button from '../../components/button';
 import Card from '../../components/card';
 import CreatePostModal from '../../components/createPostModal';
@@ -12,10 +12,14 @@ import TeachersDashboard from './teachers';
 import useAuth from '../../hooks/useAuth';
 import jwtDecode from 'jwt-decode';
 import Search from './search';
+import Student from '../cohort/students/student';
+import { getUserById, get } from '../../service/apiClient';
 
 const Dashboard = () => {
   const onPostAddedRef = useRef(null);
   const { token } = useAuth();
+  const [students, setStudents] = useState([]);
+  const [course, setCourse] = useState([]);
   
   // Safely decode token with fallback
   let decodedToken = {};
@@ -26,16 +30,43 @@ const Dashboard = () => {
   } catch (error) {
     console.error('Invalid token in Dashboard:', error);
   }
+
+  // to view people My Cohort
+  useEffect(() => {
+    async function fetchCohortData() {
+      try {
+        const token = localStorage.getItem('token');
+        if (!token) {
+          console.error('No token found.');
+          return;
+        }
+                        
+        let userId;
+        try {
+            const decodedToken = jwtDecode(token);
+            userId = decodedToken.userId;
+        } catch (decodeError) {
+            console.error('Invalid token:', decodeError);
+            return;
+        }
+        
+        const user = await getUserById(userId);
+        const data = await get(`cohorts/${user.profile.cohort.id}`);
+
+        setCourse(data.data.cohort);
+        setStudents(data.data.cohort.profiles)
+
+      } catch (error) {
+        console.error('fetchCohortData() in dashboard/index.js:', error);
+      }
+    }
+    fetchCohortData();
+  }, []);
   
   const fullName = `${decodedToken.firstName || decodedToken.first_name || 'Current'} ${decodedToken.lastName || decodedToken.last_name || 'User'}`;
   const initials = fullName?.match(/\b(\w)/g)?.join('') || 'NO';
   const  { userRole, setUserRole } = useUserRoleData();
   setUserRole(decodedToken.roleId)
-  
-  const onChange = (e) => {
-    setSearchVal(e.target.value);
-  };
-
 
   // Use the useModal hook to get the openModal and setModal functions
   const { openModal, setModal } = useModal();
@@ -55,29 +86,6 @@ const Dashboard = () => {
       onPostAddedRef.current(newPost);
     }
   };
-
-/*  TODO TRIED ADDING CORRECT INITALS TO PROFILE CIRCLE, DIDN'T WORK 
-useEffect(() => {
-    async function fetchUser() {
-      try {
-        const { userId } = jwt_decode(token || localStorage.getItem('token')) || {};
-        if (!userId) {
-          console.log('Could not determine user. Please log in again.');
-          return;
-        }
-      const fetchedUser = await get(`users/${userId}`);
-      setUser(fetchedUser);
-    } catch (error) {
-      console.error('Error fetching user:', error);
-      setUser([]);
-    }
-    const authorName = post.user.profile
-    ? `${post.user.profile.firstName || 'Unknown'} ${post.user.profile.lastName || 'User'}`
-    : 'Unknown User';
-    setUserInitials(authorName.match(/\b(\w)/g));
-  }
-      fetchUser();
-    }, []); */
 
   return (
     <>
@@ -101,6 +109,20 @@ useEffect(() => {
         { userRole === 2 ? (
            <Card>
           <h4>My Cohort</h4>
+          <p>{course.name}</p>
+          {students.map((student) => (
+            <Student
+              key={student.id || 0}
+              id ={student.id}
+              initials={`${student.firstName} ${student.lastName}`
+                  .trim()
+                  .split(/\s+/)
+                  .map(word => word[0].toUpperCase())
+                  .join('')}
+              firstName={student.firstName}
+              lastName={student.lastName}
+            />
+          ))}
         </Card>
         ) : (
           <>
