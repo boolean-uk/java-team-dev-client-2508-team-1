6273fc36ba89becc43b9509088399741deab52c8
Marie Helene Hansen <marhelhan@icloud.com>
2025-09-22 15:36:43 +0200

    ignore this

--- src/components/seeProfile/index.js
+++ src/components/seeProfile/index.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 import ProfileIcon from '../../assets/icons/profileIcon';
 
 const SeeProfile = ({initials, firstname, lastname, role, id}) => {
-  
+   
     return (
         <div className="user-panel">
           <Card>
