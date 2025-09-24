8596ab85038bc45211b361b65482fef9e9645f10
Richard-Persson <ricpersson00@gmail.com>
2025-09-24 09:48:53 +0200

    step one

--- src/pages/editCohort/index.js
+++ src/pages/editCohort/index.js
@@ -1,41 +1,14 @@
 import TextInput from "../../components/form/textInput";
 import './style.css';
+import StepOneEditCohort from "./stepOne"
 
-import Form from '../../components/form';
 const EditCohort = () =>{
 
 
   return(<>
 
+    <StepOneEditCohort/>
 
-      <Form className="welcome-form">
-
-        <div className="welcome-form-inputs">
-          <TextInput
-            name="first_name"
-            label={'First Name*'}
-            placeholder={'First Name'}
-            required
-          />
-          <TextInput 
-          name="last_name" 
-          label={'Last Name*'} 
-          placeholder={'Last Name'}
-          required />
-           <TextInput
-            name="username"
-            label={'Username*'}
-            placeholder={'Username'}
-            required
-          />
-          <TextInput
-            name="github_username"
-            label={'GitHub Username'}
-            placeholder={'GitHub Username'}
-          />
-          <p className="text-blue1">*Required</p>
-        </div>
-        </Form>
 
   </>)
 
